51f41e1cc8a454c49ccc6b8e8229d5666456862f
Jingjing Bao <JBao6@slb.com>
2025-08-31 09:16:12 +0800

    add

--- agent.py
+++ agent.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from langchain_azure_ai.chat_models import AzureAIChatCompletionsModel
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 
 def main(): 
@@ -21,13 +24,35 @@ def main():
         model=model_deployment,
     )
 
-    messages = [
-        SystemMessage(content="Translate the following from English into Italian"),
-        HumanMessage(content="hi!"),
-    ]
+    # messages = [
+    #     SystemMessage(content="Translate the following from English into Italian"),
+    #     HumanMessage(content="hi!"),
+    # ]
 
-    message_stream = model. stream(messages)
-    print("".join(chunk.content for chunk in message_stream))
+    # message_stream = model.stream(messages)
+    # print("".join(chunk.content for chunk in message_stream))
+    system_template = "Translate the following into {language}:"
+    prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", "{text}")]
+    )
+
+    producer_template = PromptTemplate(
+        template="You are an urban poet, your job is to come up \
+                verses based on a given topic.\n\
+                Here is the topic you have been asked to generate a verse on:\n\
+                {topic}",
+        input_variables=["topic"],
+    )
+
+    parser = StrOutputParser()
+
+    chain = producer_template | model | parser
+
+    # response = chain.invoke({"language": "italian", "text": "hi"})
+
+    response = chain.invoke({"topic": "living in a foreign country"})
+
+    print("response: ", response)
 
 if __name__ == '__main__': 
     main()
